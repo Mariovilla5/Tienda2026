80129da46d6fde6d1ba91b6d5ad920a8b46e1053
1dawd07 <1dawd07@A204PC28.desarrolloweb.local>
2026-01-22 14:01:52 +0100

    TIENDA2026 A 22 DE ENERO DE 2026

--- src/main/java/es/educastur/mariova62/tienda2026/Pedido.java
+++ src/main/java/es/educastur/mariova62/tienda2026/Pedido.java
@@ -1,4 +1,4 @@
-/*
+/*https://github.com/Mariovilla5/Tienda2026.git
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
